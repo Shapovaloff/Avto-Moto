7630b10707d15387b266d90785924779d40e45e3
Юрий Шаповалов <mr.Shapovaloff@gmail.com>
2020-07-14 11:05:14 +0300

    Правки замечаний наставника

--- build/js/main.js
+++ build/js/main.js
@@ -3,28 +3,17 @@
 // Функция слайдер
 
 (function () {
+  var indexSliderActive = 0;
   var sliderElement = document.querySelectorAll('.slider__item');
   var previewElemtnt = document.querySelectorAll('.preview__item');
   var prewButton = document.querySelector('.preview__btn--prew');
   var nextButton = document.querySelector('.preview__btn--next');
 
-  var findCurrentSlide = function () {
-    var index;
-    for (var i = 0; i < sliderElement.length; i++) {
-      if (sliderElement[i].classList.contains('slider__item--active')) {
-        index = i;
-      }
-    }
-    return index;
-  };
-
-  var indexSlider = findCurrentSlide();
 
   var activeSlide = function (index) {
-    for (var i = 0; i < sliderElement.length; i++) {
-      sliderElement[i].classList.remove('slider__item--active');
-    }
-
+    sliderElement.forEach(function (slider) {
+      slider.classList.remove('slider__item--active');
+    });
     sliderElement[index].classList.add('slider__item--active');
   };
 
@@ -42,22 +31,22 @@
   };
 
   var nextSlide = function () {
-    indexSlider++;
-    buttonActive(indexSlider);
-    activeSlide(indexSlider);
+    indexSliderActive++;
+    buttonActive(indexSliderActive);
+    activeSlide(indexSliderActive);
   };
 
   var prewSlide = function () {
-    indexSlider--;
-    buttonActive(indexSlider);
-    activeSlide(indexSlider);
+    indexSliderActive--;
+    buttonActive(indexSliderActive);
+    activeSlide(indexSliderActive);
   };
 
   previewElemtnt.forEach(function (item, indexItem) {
     item.addEventListener('click', function () {
-      indexSlider = indexItem;
-      buttonActive(indexSlider);
-      activeSlide(indexSlider);
+      indexSliderActive = indexItem;
+      buttonActive(indexSliderActive);
+      activeSlide(indexSliderActive);
     });
   });
 
@@ -138,52 +127,16 @@
   };
 })();
 
-
-// Функция модального окна
+// Функция открытия и загрытия модального окна
 
 (function () {
-  var newReviews = [];
-  var starRaiting = ['8', '17', '29', '37', '50', '58', '71', '79', '92', '100'];
-
-  var openButton = document.querySelector('.reviews__btn-feedback');
+  var ESCAPE = 27;
   var popup = document.querySelector('.popup__container');
+  var openButton = document.querySelector('.reviews__btn-feedback');
   var popupOverlay = document.querySelector('.popup__overlay');
   var bodyElement = document.querySelector('body');
   var closeButton = popup.querySelector('.popup__close');
-
   var fieldName = popup.querySelector('.popup__field--name');
-  var fieldAdvantages = popup.querySelector('.popup__field--advantages');
-  var fieldDisadvantages = popup.querySelector('.popup__field--disadvantages');
-  var fieldStars = document.querySelectorAll('.popup__input');
-  var fieldText = popup.querySelector('.popup__field--text');
-
-  var messageInput = document.querySelector('.popup__required-input');
-  var messageTextarea = document.querySelector('.popup__required-textarea');
-
-  var listReviews = document.querySelector('.reviews__description');
-  var popupForm = document.querySelector('.popup__form');
-  var template = document.querySelector('.template').content;
-  var newItemReviews = template.querySelector('.reviews__description-item');
-
-  var isStirageSupport = true;
-
-  var loadReviews = function () {
-    if (localStorage.getItem('newReviews') && isStirageSupport) {
-      newReviews = JSON.parse(localStorage.getItem('newReviews'));
-
-      newReviews.forEach(function (item) {
-        var reviews = newItemReviews.cloneNode(true);
-
-        reviews.querySelector('.reviews__name').textContent = item.name;
-        reviews.querySelector('.reviews__text--advantages').textContent = item.advantages;
-        reviews.querySelector('.reviews__text--disadvantages').textContent = item.disadvantages;
-        reviews.querySelector('.reviews__text--comment').textContent = item.text;
-        listReviews.appendChild(reviews);
-      });
-    }
-  };
-
-  loadReviews();
 
   var openPopup = function (evt) {
     evt.preventDefault();
@@ -192,7 +145,7 @@
     bodyElement.classList.add('no-scroll');
   };
 
-  var closePopup = function (evt) {
+  window.closePopup = function (evt) {
     evt.preventDefault();
     popupOverlay.classList.add('popup__overlay--hidden');
     popup.classList.add('popup__container--hidden');
@@ -205,83 +158,145 @@
   });
 
   closeButton.addEventListener('click', function (evt) {
-    closePopup(evt);
+    window.closePopup(evt);
   });
 
   popupOverlay.addEventListener('click', function (evt) {
-    closePopup(evt);
-  });
-
-  popupForm.addEventListener('submit', function (evt) {
-    if (!fieldName.value || !fieldText.value) {
-      evt.preventDefault();
-      if (!fieldName.value) {
-        fieldName.classList.add('popup__field--required');
-        messageInput.classList.remove('popup__required-hidden');
-      }
-
-      fieldName.addEventListener('input', function () {
-        fieldName.classList.remove('popup__field--required');
-        messageInput.classList.add('popup__required-hidden');
-      });
-
-      if (!fieldText.value) {
-        fieldText.classList.add('popup__field--required');
-        messageTextarea.classList.remove('popup__required-hidden');
-      }
-
-      fieldText.addEventListener('input', function () {
-        fieldText.classList.remove('popup__field--required');
-        messageTextarea.classList.add('popup__required-hidden');
-      });
-
-    } else {
-      evt.preventDefault();
-
-      var fieldStarsValue;
-      for (var i = 0; i < fieldStars.length; i++) {
-        if (fieldStars[i].checked) {
-          fieldStarsValue = fieldStars[i].value;
-        }
-      }
-
-      var reviews = newItemReviews.cloneNode(true);
-      reviews.querySelector('.reviews__name').textContent = fieldName.value;
-      reviews.querySelector('.reviews__text--advantages').textContent = fieldAdvantages.value;
-      reviews.querySelector('.reviews__text--disadvantages').textContent = fieldDisadvantages.value;
-      reviews.querySelector('.reviews__stars-raiting').style.width = starRaiting[fieldStarsValue - 1] + 'px';
-      reviews.querySelector('.reviews__text--comment').textContent = fieldText.value;
-      listReviews.appendChild(reviews);
-
-      if (isStirageSupport) {
-        var review = {
-          name: fieldName.value,
-          advantages: fieldAdvantages.value,
-          disadvantages: fieldDisadvantages.value,
-          star: fieldStarsValue,
-          text: fieldText.value
-        };
-
-        newReviews.push(review);
-        localStorage.setItem('newReviews', JSON.stringify(newReviews));
-      }
-
-      fieldName.value = '';
-      fieldAdvantages.value = '';
-      fieldDisadvantages.value = '';
-      fieldText.value = '';
-      fieldStars[0].checked = true;
-
-      closePopup(evt);
-    }
+    window.closePopup(evt);
   });
 
   window.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === 27) {
+    if (evt.keyCode === ESCAPE) {
       if (!popup.classList.contains('popup__container--hidden')) {
-        closePopup(evt);
+        window.closePopup(evt);
       }
     }
   });
+})();
 
+// Функция валидации формы
+
+(function () {
+  var popup = document.querySelector('.popup__container');
+  var messageInput = document.querySelector('.popup__required-input');
+  var messageTextarea = document.querySelector('.popup__required-textarea');
+  var fieldName = popup.querySelector('.popup__field--name');
+  var fieldText = popup.querySelector('.popup__field--text');
+
+  var validatesField = function (fieldsName, fieldsMessage) {
+    if (!fieldsName.value) {
+      fieldsName.classList.add('popup__field--required');
+      fieldsMessage.classList.remove('popup__required-hidden');
+    }
+  };
+
+  var requiredField = function (fieldsName, fieldsMessage) {
+    fieldsName.addEventListener('input', function () {
+      fieldsName.classList.remove('popup__field--required');
+      fieldsMessage.classList.add('popup__required-hidden');
+    });
+  };
+
+  window.validatesForm = function (evt) {
+    evt.preventDefault();
+    validatesField(fieldName, messageInput);
+    validatesField(fieldText, messageTextarea);
+  };
+
+  requiredField(fieldName, messageInput);
+  requiredField(fieldText, messageTextarea);
+})();
+
+
+// Функция отрисовки отзывов
+
+(function () {
+  var newReviews = [];
+  var starRaiting = ['8', '17', '29', '37', '50', '58', '71', '79', '92', '100'];
+  var popup = document.querySelector('.popup__container');
+
+  var fieldName = popup.querySelector('.popup__field--name');
+  var fieldAdvantages = popup.querySelector('.popup__field--advantages');
+  var fieldDisadvantages = popup.querySelector('.popup__field--disadvantages');
+  var fieldStars = popup.querySelectorAll('.popup__input');
+  var fieldText = popup.querySelector('.popup__field--text');
+
+  var listReviews = document.querySelector('.reviews__description');
+  var reviewsItem = listReviews.querySelector('.reviews__description-item');
+  var popupForm = document.querySelector('.popup__form');
+
+  var renderReviews = function (item) {
+    var reviews = reviewsItem.cloneNode(true);
+    reviews.querySelector('.reviews__name').textContent = item.name;
+    reviews.querySelector('.reviews__text--advantages').textContent = item.advantages;
+    reviews.querySelector('.reviews__text--disadvantages').textContent = item.disadvantages;
+    reviews.querySelector('.reviews__stars-raiting').style.width = starRaiting[item.star] + 'px';
+    reviews.querySelector('.reviews__text--comment').textContent = item.text;
+    listReviews.appendChild(reviews);
+  };
+
+  var isStorageSupport = true;
+  var storage = '';
+
+  try {
+    storage = localStorage.getItem('newReviews');
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  var loadReviews = function () {
+    if (storage && isStorageSupport) {
+      newReviews = JSON.parse(storage);
+
+      newReviews.forEach(function (item) {
+        renderReviews(item);
+      });
+    }
+  };
+
+  loadReviews();
+
+  var saveReviews = function () {
+    if (isStorageSupport) {
+      localStorage.setItem('newReviews', JSON.stringify(newReviews));
+    }
+  };
+
+  var showReviews = function (evt) {
+    evt.preventDefault();
+    var fieldStarsValue;
+
+    fieldStars.forEach(function (star) {
+      if (star.checked) {
+        fieldStarsValue = star.value - 1;
+      }
+    });
+
+    var review = {
+      name: fieldName.value,
+      advantages: fieldAdvantages.value,
+      disadvantages: fieldDisadvantages.value,
+      star: fieldStarsValue,
+      text: fieldText.value
+    };
+
+    fieldName.value = '';
+    fieldAdvantages.value = '';
+    fieldDisadvantages.value = '';
+    fieldText.value = '';
+    fieldStars[0].checked = true;
+    newReviews.push(review);
+
+    renderReviews(review);
+    saveReviews();
+  };
+
+  popupForm.addEventListener('submit', function (evt) {
+    if (!fieldName.value || !fieldText.value) {
+      window.validatesForm(evt);
+    } else {
+      showReviews(evt);
+      window.closePopup(evt);
+    }
+  });
 })();
